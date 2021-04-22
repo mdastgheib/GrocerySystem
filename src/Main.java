291b2ddb68b63fa1e85944435aaad0a313c475d7
Moussa Dastgheib <zyenaux@gmail.com>
2021-04-21 23:08:40 -0500

    Created a few new files in order to run fxml. Initated first controller and fxml, as well as created .css class for themeing.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/Style/fxmls/main.fxml"));
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
+
     }
 
-
     public static void main(String[] args) {
+
         launch(args);
     }
 }
