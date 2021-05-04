1f5fb11c604ed9fc51a1f502684110469cff4121
Moussa Dastgheib <zyenaux@gmail.com>
2021-05-04 16:25:01 -0500

    Adjusted FXML pane sizes, created multiple new stages for user type logins.

--- src/mainController.java
+++ src/mainController.java
@@ -1,5 +1,8 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
@@ -19,7 +22,7 @@ public class mainController {
     public String user = "";
 
     // public static int loggingIn(String k1, int v1) throws IOException {
-    public void loggingIn() throws IOException {
+    public void logIn(ActionEvent event) throws IOException {
         String k1 = username.getText();
         String v1 = password.getText();
         this.user = k1;
@@ -43,20 +46,31 @@ public class mainController {
         }
         String temp = (String) username.get(k1);
         // THIS MEANS A VALID USERNAME AND PASSWORD COMBINATION WAS ENTERED GOING TO MAKE TWO SCENES BASED OFF WHETHER OR NOT THIS PERSON IS AN ADMINISTATOR
-        if (username.containsKey(k1) &&  temp.equals(v1)){
-            FXMLLoader User = new FXMLLoader(getClass().getResource("/Style/fxmls/list.fxml"));
-            AnchorPane aPane = User.load();
-            Scene userScene = new Scene(aPane,600,600);
-            Stage currentStage = new Stage();
-            currentStage.setScene(userScene);
+
+        if (username.containsKey(k1) &&  temp.equals(v1))
+        {
+            //Loading in our fxmls for each member (each user has different fxml)
+            Parent managerList = FXMLLoader.load(getClass().getResource("/Style/fxmls/list.fxml"));
+            //Parent shopperList = FXMLLoader.load(getClass().getResource("/Style/fxmls/list.fxml"));
+            //Parent driverList = FXMLLoader.load(getClass().getResource("/Style/fxmls/list.fxml"));
+
+            Scene manager = new Scene(managerList);
+            //Scene shopper = new Scene(managerList);
+            //Scene driver = new Scene(managerList);
+
+            //Obtaining stage information and setting our new scene/fxml
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.setScene(manager);
             currentStage.show();
 
-            Alert notice = new Alert(Alert.AlertType.CONFIRMATION, "Successfully logged in, thank you. ");
-            notice.showAndWait();
-        }else {
-            Alert notice = new Alert(Alert.AlertType.ERROR, "UserName or Password is Incorrect. ");
+
+            Alert notice = new Alert(Alert.AlertType.CONFIRMATION, "Login Successful.");
             notice.showAndWait();
         }
+        else {
+            Alert notice = new Alert(Alert.AlertType.ERROR, "The Username or password is incorrect!");
+            notice.showAndWait();
+         }
 
     }
 }
