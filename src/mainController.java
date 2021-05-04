351f2c0c6cd1e2669d7a9a210c9051cc0cf8bbb5
KhalidFarhoud <70921949+KhalidFarhoud@users.noreply.github.com>
2021-05-04 16:59:39 -0500

    Implemented The "taking" method which will take from items that are currently in our list, NOTE item needs to exists in order to take

--- src/mainController.java
+++ src/mainController.java
@@ -32,36 +32,70 @@ public class mainController {
             return;
         }
         // declaring my HashMap
-        HashMap<Object, Object> username = new HashMap<>();
-        File file = new File("username.properties");
+        HashMap<Object, Object> customer = new HashMap<>();
+        File file = new File("customer.properties");
         FileInputStream reader = new FileInputStream(file);
         Properties properties = new Properties();
         properties.load(reader);
         reader.close();
 
+        HashMap<Object, Object> driver = new HashMap<>();
+        File file1 = new File("driver.properties");
+        FileInputStream reader1 = new FileInputStream(file1);
+        Properties properties1 = new Properties();
+        properties1.load(reader1);
+        reader.close();
+
+        HashMap<Object, Object> username = new HashMap<>();
+        File file2 = new File("username.properties");
+        FileInputStream reader2 = new FileInputStream(file2);
+        Properties properties2 = new Properties();
+        properties2.load(reader2);
+        reader.close();
+
         // filling the HashMap with the property file is empty nothing will be in it
         // but the following if statement will fill it if anything is passed
         for(String mainKeys: properties.stringPropertyNames()){
-            username.put(mainKeys, properties.get(mainKeys));
+            customer.put(mainKeys, properties.get(mainKeys));
         }
-        String temp = (String) username.get(k1);
+        for(String mainKeys: properties1.stringPropertyNames()){
+            driver.put(mainKeys, properties1.get(mainKeys));
+        }
+        for(String mainKeys: properties2.stringPropertyNames()){
+            username.put(mainKeys, properties2.get(mainKeys));
+        }
+        String usr = (String) username.get(k1);
+        String customers = (String) customer.get(k1);
+        String drivers = (String) driver.get(k1);
         // THIS MEANS A VALID USERNAME AND PASSWORD COMBINATION WAS ENTERED GOING TO MAKE TWO SCENES BASED OFF WHETHER OR NOT THIS PERSON IS AN ADMINISTATOR
 
-        if (username.containsKey(k1) &&  temp.equals(v1))
+        if ((username.containsKey(k1) &&  usr.equals(v1)) || (driver.containsKey(k1) &&  drivers.equals(v1)) || (customer.containsKey(k1) &&  customers.equals(v1)))
         {
+
             //Loading in our fxmls for each member (each user has different fxml)
             Parent managerList = FXMLLoader.load(getClass().getResource("/Style/fxmls/list.fxml"));
             //Parent shopperList = FXMLLoader.load(getClass().getResource("/Style/fxmls/list.fxml"));
             //Parent driverList = FXMLLoader.load(getClass().getResource("/Style/fxmls/list.fxml"));
 
             Scene manager = new Scene(managerList);
-            //Scene shopper = new Scene(managerList);
-            //Scene driver = new Scene(managerList);
+           // Scene shopper = new Scene(managerList);
+           // Scene delivery = new Scene(managerList);
 
             //Obtaining stage information and setting our new scene/fxml
             Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            currentStage.setScene(manager);
-            currentStage.show();
+            if(username.containsKey(k1)) {
+                currentStage.setScene(manager);
+                currentStage.show();
+            }
+            else if(driver.containsKey(k1)) {
+                //currentStage.setScene(delivery);
+                //currentStage.show();
+                System.out.println("THIS WILL BE THE DRIVER FXML");
+            } else {
+                //currentStage.setScene(shopper);
+                //currentStage.show();
+                System.out.println("THIS WILL BE THE CUSTOMER FXML");
+            }
 
 
             Alert notice = new Alert(Alert.AlertType.CONFIRMATION, "Login Successful.");
