3d0954baa19a8c1a7d50e1956a20b035f5eac969
Moussa Dastgheib <zyenaux@gmail.com>
2021-05-05 22:52:38 -0500

    Transaction history now displaying via listview.

--- src/transactionsController.java
+++ src/transactionsController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -21,38 +22,34 @@ import java.util.ResourceBundle;
 public class transactionsController extends managerController {
 
     @FXML
-    private TableView<Table> fxTable;
-    @FXML
     private TableColumn<Table, String> column_history;
+    @FXML
+    ListView<String> list;
 
-    ObservableList<Table> oblist;
+    ObservableList<String> items = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        list.setItems(items);
         try
         {
             Connection con = MySQLConnection.getConnection();
-            oblist = FXCollections.observableArrayList();
+
             ResultSet rs = con.createStatement().executeQuery("select * from Transactions");
 
             while (rs.next()) {
-                oblist.add(new Table(rs.getString(1)));
+                items.add((rs.getString(1)));
             }
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
-        column_history.setCellValueFactory(new PropertyValueFactory<>("History"));
-
-        fxTable.setItems(null);
-        fxTable.setItems(oblist);
-
     }
 
     public void returnMain(ActionEvent event) throws IOException {
-        Parent mainPage = FXMLLoader.load(getClass().getResource("/Style/fxmls/main.fxml"));
+        Parent mainPage = FXMLLoader.load(getClass().getResource("/Style/fxmls/manager.fxml"));
         Scene main = new Scene(mainPage);
         //Obtaining stage information and setting our new scene/fxml
         Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
